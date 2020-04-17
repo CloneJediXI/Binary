68322a50f1b60b117f910512c8426d04a9103f59
Spencer Burke <sburke17@emich.edu>
2020-04-17 14:38:52 -0400

    Working splash Screen and new colors

--- app/src/main/java/emu/cosc426/binaryconverter/Splash.java
+++ app/src/main/java/emu/cosc426/binaryconverter/Splash.java
@@ -1,9 +1,20 @@
 package emu.cosc426.binaryconverter;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.gif.GifDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 public class Splash extends AppCompatActivity {
 
@@ -11,12 +22,39 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        final ImageView image = findViewById(R.id.gif);
+
+
+        /*Glide.with(this)
+                .load(R.drawable.natsco)
+                .into(image);*/
+
+        Glide.with(this).asGif().load(R.drawable.natsco).listener(new RequestListener<GifDrawable>() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<GifDrawable> target, boolean isFirstResource) {
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(GifDrawable resource, Object model, Target<GifDrawable> target, DataSource dataSource, boolean isFirstResource) {
+                resource.setLoopCount(1);
+                resource.registerAnimationCallback(new Animatable2Compat.AnimationCallback() {
+                    @Override
+                    public void onAnimationEnd(Drawable drawable) {
+                        image.setImageResource(R.drawable.natscoletters);
+                    }
+                });
+                return false;
+            }
+        }).into(image);
+
+
         Thread myThread = new Thread()
         {
             @Override
             public void run() {
                 try {
-                    sleep(3200);
+                    sleep(5000);
                     Intent intent = new Intent(getApplicationContext(),MainActivity.class);
                     startActivity(intent);
                     finish();
